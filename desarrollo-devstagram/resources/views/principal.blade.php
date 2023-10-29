87bababd73dd2a247877a75d62f3cf714a33cd29
Antonio Estudillo <jesusestudillo123@gmail.com>
2023-10-29 16:46:14 -0600

    Login completo

---
+++ desarrollo-devstagram/resources/views/principal.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+        <title>Devstagram</title>
+        @vite('resources/css/app.css')
+
+    </head>
+    <body>
+        @section('titulo')
+            Hola
+        @endsection
+        
+    </body>
+</html>
---
+++ desarrollo-devstagram/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('principal');
+});
+
+Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
+Route::post('/crear-cuenta', [RegisterController::class, 'store']);
+
+Route::get('login', [LoginController::class, 'index'])->name('login');
+Route::get('login', [LoginController::class, 'store']);
+
+Route::get('/muro', [PostController::class, 'index'])->name('post.index');
